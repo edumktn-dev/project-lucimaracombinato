9b8a44d8b59c1607b043f7d1d34a21f83c130d3f
edumktn-dev <edu.mktn@gmail.com>
2025-08-20 23:26:16 -0300

    Corrigir ortografia e acentuação

--- blog.js
+++ blog.js
@@ -7,19 +7,19 @@ const posts = [
 
 Luto é quando perdemos algo ou alguém que nos era muito amado e valioso. A perda de um emprego e o fim de um relacionamento também são considerados luto. Vivemos um tempo onde é comum medicar a pessoa que está enlutada, apesar do luto não ser doença. Os casos que necessitam de medicação devem ser avaliados e acompanhados por um psiquiatra.
 
-Os sintomas mais comuns do luto são: dificuldades para dormir, dificuldades para se alimentar, dores no corpo, sistema imunológico fragilizado, choro, raiva, tristeza, desanimo, desamparo, insegurança, falta de concentração e sentimentos de culpa.
+Os sintomas mais comuns do luto são: dificuldades para dormir, dificuldades para se alimentar, dores no corpo, sistema imunológico fragilizado, choro, raiva, tristeza, desânimo, desamparo, insegurança, falta de concentração e sentimentos de culpa.
 
-Dra. Elisabeth Kubler Ross descreve cinco fases do luto são elas: negação, raiva, barganha, depressão e aceitação. Mas o luto não é uma corrida de obstáculos e cada um deve vive-lo a sua maneira.
+Dra. Elisabeth Kubler Ross descreve cinco fases do luto são elas: negação, raiva, barganha, depressão e aceitação. Mas o luto não é uma corrida de obstáculos e cada um deve vivê-lo à sua maneira.
 
 Dra. Sarah Vieira compara o luto com uma montanha russa no escuro... Ninguém sabe a hora que vai cair, pode estar tudo bem, de repente uma música, um cheiro te traz uma lembrança.
 
-Apesar da morte ser a única certeza que temos, ela causa muito espanto. A gente tenta se esconder e se afastar da morte, como se ela não fosse parte da vida, procuramos um culpado tentando manter o controle da situação e não encarramos o fato de que a morte sempre foi inevitável.
+Apesar da morte ser a única certeza que temos, ela causa muito espanto. A gente tenta se esconder e se afastar da morte, como se ela não fosse parte da vida, procuramos um culpado tentando manter o controle da situação e não encaramos o fato de que a morte sempre foi inevitável.
 
-Jung traz a morte como uma terrível brutalidade, às pessoas são arrancadas de nós de forma muito abrupta. A pessoa que mais amamos ou a única pessoa que amávamos. E a resposta mais comum é desespero na ausência é revela a importância que tinha a presença.
+Jung traz a morte como uma terrível brutalidade, as pessoas são arrancadas de nós de forma muito abrupta. A pessoa que mais amamos ou a única pessoa que amávamos. E a resposta mais comum é desespero na ausência é revela a importância que tinha a presença.
 
-Eu comparo perder um ente querido a perder um membro do corpo: um braço uma perna. Aconteceu uma ruptura, cortou um pouco de si. E o que resta é o silencio.
+Eu comparo perder um ente querido a perder um membro do corpo: um braço uma perna. Aconteceu uma ruptura, cortou um pouco de si. E o que resta é o silêncio.
 
-O que devemos fazer? Reabitar-se, reinventar-se, costurar os pedaços, sobreviver. O luto exige mudanças, mudança de conceitos, de postura, de valores.
+O que devemos fazer? Reabilitar-se, reinventar-se, costurar os pedaços, sobreviver. O luto exige mudanças, mudança de conceitos, de postura, de valores.
 
 O luto exige reflexões sobre o outro lado da vida e sobre o sentido dessa vida, saímos modificados dessa experiência construímos outro conceito de vida e de morte. Fica evidente que não há tempo a perder.
 
@@ -36,26 +36,26 @@ O luto é uma experiência individual, porém, não deve ser vivido sozinho o ap
         date: "2025-08-14", 
         text: `**Quando devo procurar terapia?**
 
-Parece uma pergunta boba, que merece uma resposta obvia: quando eu tiver vontade ou quando sentir necessidade de falar com um especialista sobre algo.
+Parece uma pergunta boba, que merece uma resposta óbvia: quando eu tiver vontade ou quando sentir necessidade de falar com um especialista sobre algo.
 
-Alem disso, a psicoterapia e um espaco de acolhimento, etico, sem expectativas ou cobrancas. Imagine que maravilhoso e se abrir com alguem que te acolhe e nao fica jogando as coisas na sua cara ou cobrando atitudes. "Mas voce nao largou dele ainda?" "Por que voce fica nesse emprego se reclama tanto?" "Mas, tambem, voce nao da limites - e isso que acontece."
+Além disso, a psicoterapia é um espaço de acolhimento, ético, sem expectativas ou cobranças. Imagine que maravilhoso é se abrir com alguém que te acolhe e não fica jogando as coisas na sua cara ou cobrando atitudes. "Mas você não largou dele ainda?" "Por que você fica nesse emprego se reclama tanto?" "Mas, também, você não dá limites - é isso que acontece."
 
-E a pior coisa e o "discurso do heroi": a pessoa diz o que voce precisa fazer como se fosse muito facil...
+E a pior coisa é o "discurso do herói": a pessoa diz o que você precisa fazer como se fosse muito fácil...
 
-Porem, mais do que ser aceito e compreendido, fazer psicoterapia e aprender a responsabilizar-se e a sentir-se confortavel com isso; afinal, se eu nao sou responsavel, eu nao mudo nada na minha vida.
+Porém, mais do que ser aceito e compreendido, fazer psicoterapia é aprender a responsabilizar-se e a sentir-se confortável com isso; afinal, se eu não sou responsável, eu não mudo nada na minha vida.
 
-Gosto muito de uma pergunta que Viktor Frankl faz em seu livro Em busca de sentido. Eu reproduzo essa pergunta a varios de meus pacientes: "O que voce espera da vida?". Depois da reflexao e da resposta, digo que a pergunta esta errada; a pergunta correta e: "O que a vida espera de voce?". Quando voce muda a pergunta, a pessoa perde a passividade e se torna ativa - ei, a vida esta esperando de voce... E um sentido para a vida nao se encontra virando uma esquina; um sentido para a vida a gente constroi.
+Gosto muito de uma pergunta que Viktor Frankl faz em seu livro Em busca de sentido. Eu reproduzo essa pergunta a vários de meus pacientes: "O que você espera da vida?". Depois da reflexão e da resposta, digo que a pergunta está errada; a pergunta correta é: "O que a vida espera de você?". Quando você muda a pergunta, a pessoa perde a passividade e se torna ativa - ei, a vida está esperando de você... E um sentido para a vida não se encontra virando uma esquina; um sentido para a vida a gente constrói.
 
-Uma pessoa esta pronta para a psicoterapia quando pensa: "O que eu faco agora?". Sabe aquela frase famosa do Sartre: "Nao importa o que fizeram com voce; o que importa e o que voce faz com aquilo que fizeram com voce". Um momento de angustia e indecisao. O que eu faco com esse diagnostico? O que eu faco com essa deslealdade? O que eu faco com tanta rejeicao? O que eu faco com essa solidao? O que eu faco com meus lutos e cicatrizes?
+Uma pessoa está pronta para a psicoterapia quando pensa: "O que eu faço agora?". Sabe aquela frase famosa do Sartre: "Não importa o que fizeram com você; o que importa é o que você faz com aquilo que fizeram com você". Um momento de angústia e indecisão. O que eu faço com esse diagnóstico? O que eu faço com essa deslealdade? O que eu faço com tanta rejeição? O que eu faço com essa solidão? O que eu faço com meus lutos e cicatrizes?
 
-Conhece a matriosca? E uma bonequinha russa com varias bonecas iguais, porem de tamanhos diferentes; uma se encaixa dentro da outra. Fazemos psicoterapia para isso tambem: para entender que so preenchemos a nos mesmos com nos mesmos - com a nossa crianca, que precisa ser ouvida, que precisa de atencao e carinho; preenchemos com o nosso adolescente, muitas vezes revoltado com a vida que estamos levando - nao era isso que ele queria.
+Conhece a matriosca? É uma bonequinha russa com várias bonecas iguais, porém de tamanhos diferentes; uma se encaixa dentro da outra. Fazemos psicoterapia para isso também: para entender que só preenchemos a nós mesmos com nós mesmos - com a nossa criança, que precisa ser ouvida, que precisa de atenção e carinho; preenchemos com o nosso adolescente, muitas vezes revoltado com a vida que estamos levando - não era isso que ele queria.
 
-Uma frase que escuto muito e com a qual nao concordo e: "Todo mundo precisa de psicoterapia". Para a psicoterapia funcionar, a pessoa precisa sentir que o que esta acontecendo na vida dela pode ser resolvido conversando. E quase magico.
+Uma frase que escuto muito e com a qual não concordo é: "Todo mundo precisa de psicoterapia". Para a psicoterapia funcionar, a pessoa precisa sentir que o que está acontecendo na vida dela pode ser resolvido conversando. É quase mágico.
 
-Para finalizar, quando eu era estudante, estavamos discutindo essa questao em sala de aula; nos, alunos inseguros, tentando dar uma resposta mais complexa que a outra, e a professora sabiamente respondeu que fazemos psicoterapia para aprender a amar - a amar os pais com todas as suas imperfeicoes; amar e perdoar a nos mesmos; amar a vida.
+Para finalizar, quando eu era estudante, estávamos discutindo essa questão em sala de aula; nós, alunos inseguros, tentando dar uma resposta mais complexa que a outra, e a professora sabiamente respondeu que fazemos psicoterapia para aprender a amar - a amar os pais com todas as suas imperfeições; amar e perdoar a nós mesmos; amar a vida.
 
 **Lucimara Combinato**
-**Psicologa**`, 
+**Psicóloga**`, 
         image: null, 
         likes: 15 
     }
